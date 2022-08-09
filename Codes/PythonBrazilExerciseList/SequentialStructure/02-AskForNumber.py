18faffb26f501beafe6155e9402f6d0e1406c22f
JEsidio <jenifer_esidio@hotmail.com>
2022-08-08 23:08:36 -0300

    First exercises

---
+++ Codes/PythonBrazilExerciseList/SequentialStructure/02-AskForNumber.py
@@ -0,0 +1,4 @@
+# Write a Program that asks for a number and then displays the message The number entered was [number].
+
+number = input('Enter a number: ')
+print(f'The number entered was {number}.')
---
+++ Codes/PythonBrazilExerciseList/SequentialStructure/03-SumNumbers.py
@@ -0,0 +1,8 @@
+# Write a program that asks for two numbers and prints the sum.
+
+number1 = float(input('Enter the first number: '))
+number2 = float(input('Enter the second number: '))
+
+sum = number1 + number2
+
+print(f'The Sum of the numbers {number1} + {number2} is: {sum}')
